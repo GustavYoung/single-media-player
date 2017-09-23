98ab5fe34e113e45db96c511aedc1f042db8fb27
Gustav_Young <gus.santana@icloud.com>
2017-09-23 18:30:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+// ./main.js
+const {app, BrowserWindow} = require('electron')
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1920, height: 1080});
+
+  // Specify entry point
+  win.loadURL('http://localhost:4200');
+
+  // Show dev tools
+  // Remove this line before distributing
+  win.maximize();
+  win.setFullScreen(true);
+  //win.webContents.openDevTools();
+  win.setFullScreen(true);
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
